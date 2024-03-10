ebd472d7bbb53dd4d0bb697b5689def6184663f3
andreiChabatul <andreichabatul@gmail.com>
2024-03-10 22:07:01 +0300

    feat task item date

--- app/asana/src/app/tasks/components/task-date/task-date.component.ts
+++ app/asana/src/app/tasks/components/task-date/task-date.component.ts
@@ -1,13 +1,20 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
+import { MatDatepickerInputEvent } from '@angular/material/datepicker';
 
 @Component({
   selector: 'app-task-date',
   templateUrl: './task-date.component.html',
-  styleUrls: ['./task-date.component.scss']
+  styleUrls: ['./task-date.component.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class TaskDateComponent {
 
   @Input() deadline?: Date;
   @Input() id: string;
+  date: Date;
+
+  onDate(event: MatDatepickerInputEvent<any, any>): void {
+    console.log(event.value )
+  }
 
 }
