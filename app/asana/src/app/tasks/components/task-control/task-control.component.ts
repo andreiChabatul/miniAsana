dad342580edc9ab8ce75d956bc6e40e42f9d7ebc
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-11 11:50:21 +0300

    feat: add compnent task control

---
+++ app/asana/src/app/tasks/components/task-control/task-control.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-task-control',
+  templateUrl: './task-control.component.html',
+  styleUrl: './task-control.component.scss'
+})
+export class TaskControlComponent {
+
+}
