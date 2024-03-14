a67f0a6e6e9678c359a352235d9d101b5a1ede1f
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-14 09:24:25 +0300

    fix tasks module

--- app/asana/src/app/tasks/tasks.module.ts
+++ app/asana/src/app/tasks/tasks.module.ts
@@ -2,48 +2,47 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksPageComponent } from './page/tasks-page/tasks-page.component';
 import { TaskItemComponent } from './components/task-item/task-item.component';
-import { TaskDateComponent } from './components/task-date/task-date.component';
 import { MaterialsModule } from '../materials/materials.module';
-import { BackDateDirective } from './directive/backgroundDate';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { TaskcheckBoxComponent } from './components/taskcheck-box/taskcheck-box.component';
-import { TaskStatusComponent } from './components/task-status/task-status.component';
-import { BackStatusDirective } from './directive/backgroundStatus';
-import { TaskPriorityComponent } from './components/task-priority/task-priority.component';
-import { BackPriorityDirective } from './directive/backgroundPriority';
-import { TaskControlComponent } from './components/task-control/task-control.component';
-import { TaskAssigneeComponent } from './components/task-assignee/task-assignee.component';
-import { TaskTitleComponent } from './components/task-title/task-title.component';
 import { TaskWrapperComponent } from './components/task-wrapper/task-wrapper.component';
 import { TaskPanelItemComponent } from './components/task-panel-item/task-panel-item.component';
-
+import { HeaderTasksComponent } from './components/header-tasks/header-tasks.component';
+import { HeaderGroupComponent } from './components/header-group/header-group.component';
+import { HeaderOrderComponent } from './components/header-order/header-order.component';
+import { HeaderSearchComponent } from './components/header-search/header-search.component';
+import { SeacrhTaskPipe } from './pipe/seacrh-task.pipe';
+import { TasksHatComponent } from './components/tasks-hat/tasks-hat.component';
+import { TaskNewComponent } from './components/task-new/task-new.component';
+import { SharedModule } from '../shared/shared.module';
+import { SortOrderTaskServices } from './services/sort.order.service';
 
 
 @NgModule({
   declarations: [
     TasksPageComponent,
     TaskItemComponent,
-    TaskDateComponent,
-    BackDateDirective,
-    TaskcheckBoxComponent,
-    TaskStatusComponent,
-    BackStatusDirective,
-    TaskPriorityComponent,
-    BackPriorityDirective,
-    TaskControlComponent,
-    TaskAssigneeComponent,
-    TaskTitleComponent,
     TaskWrapperComponent,
-    TaskPanelItemComponent
+    TaskPanelItemComponent,
+    HeaderTasksComponent,
+    HeaderGroupComponent,
+    HeaderOrderComponent,
+    HeaderSearchComponent,
+    SeacrhTaskPipe,
+    TasksHatComponent,
+    TaskNewComponent
   ],
   imports: [
     CommonModule,
     MaterialsModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    SharedModule
   ],
   exports: [
     TasksPageComponent
+  ],
+  providers: [
+    SortOrderTaskServices
   ]
 })
 export class TasksModule { }
