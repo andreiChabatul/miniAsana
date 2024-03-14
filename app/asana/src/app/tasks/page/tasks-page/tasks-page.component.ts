3564961902f7ba65bd1c301ceae9af24993d6f25
Andrei Chabatul <andreichabatul@gmail.com>
2024-03-14 09:25:06 +0300

    fix

--- app/asana/src/app/tasks/page/tasks-page/tasks-page.component.ts
+++ app/asana/src/app/tasks/page/tasks-page/tasks-page.component.ts
@@ -1,14 +1,24 @@
-import { Component } from '@angular/core';
-import { USERS } from 'src/fakeDB/users';
-import { EPriority, EStatus, ITask } from 'src/types/task';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { selectTasks } from 'src/store/selectors';
+import { AppStore } from 'src/types/store';
+import { ITask } from 'src/types/task';
+import { TasksServices } from '../../../shared/services/tasks.services';
 
 @Component({
   selector: 'app-tasks-page',
   templateUrl: './tasks-page.component.html',
   styleUrls: ['./tasks-page.component.scss']
 })
-export class TasksPageComponent {
+export class TasksPageComponent implements OnInit {
 
-  
+  tasks$: Observable<ITask[]> = this.store.select(selectTasks);
+
+  constructor(private store: Store<AppStore>, private taskServices: TasksServices) { }
+
+  ngOnInit(): void {
+    this.taskServices.getAllTasks();
+  }
 
 }
